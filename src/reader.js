1ee476c92e9fc9f2fb64ef7a1f8735d40a210f13
Kiril Vatev <vatev.1@gmail.com>
2020-06-13 15:56:42 -0400

    reading jpeg from raw file is one is present

--- src/reader.js
+++ src/reader.js
@@ -2,6 +2,14 @@
 
 const IMAGES = ['image/jpeg', 'image/png'];
 
+const get = (obj = {}, [key, ...path] = [], fallback = undefined) => {
+  if (key in obj && path.length) {
+    return get(obj[key], path, fallback);
+  }
+
+  return key in obj ? obj[key] : fallback;
+};
+
 function toBase64(buffer) {
   let binary = '';
   const bytes = new Uint8Array(buffer);
@@ -13,12 +21,27 @@ function toBase64(buffer) {
   return window.btoa(binary);
 }
 
+function getEmbeddedImage(exif, arrayBuffer) {
+  const offset = get(exif, ['StripOffsets', 'description']);
+  const length = get(exif, ['StripByteCounts', 'description']);
+
+  return offset && length ?
+    arrayBuffer.slice(offset, offset + length) :
+    null;
+}
+
 export default async file => {
   const { name, type, size } = file;
 
   const arrayBuffer = await file.arrayBuffer();
   const tags = ExifReader.load(arrayBuffer, { expanded: true });
-  const url = `data:image/jpg;base64,${ IMAGES.includes(type) ? toBase64(arrayBuffer) : tags['Thumbnail'].base64}`;
+
+  const jpg = getEmbeddedImage(tags.exif, arrayBuffer);
+  const data = jpg ? toBase64(jpg) :
+    IMAGES.includes(type) ? toBase64(arrayBuffer) :
+      tags.Thumbnail.base64;
+
+  const url = `data:image/jpg;base64,${data}`;
 
   return { name, type, size, tags, url };
 };
