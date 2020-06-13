2a398bb3ff97c75be9544e07ef029ec58ffd2d4e
Kiril Vatev <vatev.1@gmail.com>
2020-06-13 15:29:08 -0400

    allowing supported images to be displayed directory

--- src/reader.js
+++ src/reader.js
@@ -1,11 +1,24 @@
 /* globals ExifReader */
 
+const IMAGES = ['image/jpeg', 'image/png'];
+
+function toBase64(buffer) {
+  let binary = '';
+  const bytes = new Uint8Array(buffer);
+
+  for (let i = 0, l = bytes.byteLength; i < l; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+
+  return window.btoa(binary);
+}
+
 export default async file => {
   const { name, type, size } = file;
 
   const arrayBuffer = await file.arrayBuffer();
   const tags = ExifReader.load(arrayBuffer, { expanded: true });
-  const url = `data:image/jpg;base64,${tags['Thumbnail'].base64}`;
+  const url = `data:image/jpg;base64,${ IMAGES.includes(type) ? toBase64(arrayBuffer) : tags['Thumbnail'].base64}`;
 
   return { name, type, size, tags, url };
 };
